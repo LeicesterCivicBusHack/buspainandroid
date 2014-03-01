48c9cf54c7a03c7b5242c9418d8eda10b10f4082
You Li <yl291@student.le.ac.uk>
2014-03-01 14:55:50 +0000

    init UI

--- buspain/src/uk/co/civichack/buspain/MainActivity.java
+++ buspain/src/uk/co/civichack/buspain/MainActivity.java
@@ -3,13 +3,28 @@ package uk.co.civichack.buspain;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 public class MainActivity extends Activity {
+
+	private Button late = null;
+	private Button arrive = null;
+	private Button start = null;
+	private Button reset = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TimePicker t = (TimePicker) this.findViewById(R.id.timePicker1);
+		t.setIs24HourView(true);
+
+		late = (Button) this.findViewById(R.id.button1);
+		arrive = (Button) this.findViewById(R.id.button2);
+		start = (Button) this.findViewById(R.id.start);
+		reset = (Button) this.findViewById(R.id.bus_reset);
+
 	}
 
 	@Override
