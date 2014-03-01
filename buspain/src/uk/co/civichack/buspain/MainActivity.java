b6ca9760bd40f9dd92885117092aee8b96002d7d
You Li <yl291@student.le.ac.uk>
2014-03-01 15:01:18 +0000

    add complaint

--- buspain/src/uk/co/civichack/buspain/MainActivity.java
+++ buspain/src/uk/co/civichack/buspain/MainActivity.java
@@ -3,6 +3,8 @@ package uk.co.civichack.buspain;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TimePicker;
 
@@ -24,6 +26,31 @@ public class MainActivity extends Activity {
 		arrive = (Button) this.findViewById(R.id.button2);
 		start = (Button) this.findViewById(R.id.start);
 		reset = (Button) this.findViewById(R.id.bus_reset);
+
+		setListeners();
+	}
+
+	private void setListeners() {
+		
+		arrive.setEnabled(false);
+		late.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				late.setEnabled(false);
+				arrive.setEnabled(true);
+			}
+			
+		});
+		arrive.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				late.setEnabled(true);
+				arrive.setEnabled(false);
+			}
+			
+		});
 
 	}
 
